3f5b817387486444c8446d3ea9392bec64ad6590
tofaelkabir <tofaelkabir@gmail.com>
2021-08-05 22:59:45 -0400

    08052021 10:59pm

---
+++ src/lec19_java_loop/Use_of_java_continue.java
@@ -0,0 +1,10 @@
+package lec19_java_loop;
+
+public class Use_of_java_continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/lec19_java_loop/Use_of_java_for_each_loop.java
@@ -0,0 +1,10 @@
+package lec19_java_loop;
+
+public class Use_of_java_for_each_loop {
+
+	public static void main(String[] args) {
+		// TODO Later when we will learn collection framework, kept here for record
+
+	}
+
+}
